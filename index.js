9750b5b59c3ac940e689f6f3f09b9dbffb16985d
Felipe L Tavares <felipelimatavares96@gmail.com>
2022-04-22 23:15:23 -0300

    6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-/* import helmet from 'helmet'; */
+import helmet from 'helmet';
 import expressSanitizer from 'express-sanitizer';
 import 'dotenv/config';
 
@@ -12,8 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '4mb'}));
 app.use(express.urlencoded({extended: true, limit: '4mb'}));
-/* app.use(helmet()); */
+app.use(helmet());
 app.use(expressSanitizer());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', /* 'http://localhost:3000' */ 'https://loja-teste-frontend.vercel.app/' );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  return next();
+});
 
 app.use('/api', Router);
 
