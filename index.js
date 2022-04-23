b3715f71d24999ad7cde66916c79a1e5f3965e05
Felipe L Tavares <felipelimatavares96@gmail.com>
2022-04-22 23:06:59 -0300

    9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
+/* import helmet from 'helmet'; */
 import expressSanitizer from 'express-sanitizer';
 import 'dotenv/config';
 
@@ -9,11 +9,11 @@ import Router from './router.js';
 //Server
 const app = express();
 //Config
+app.use(cors());
 app.use(express.json({limit: '4mb'}));
 app.use(express.urlencoded({extended: true, limit: '4mb'}));
-app.use(helmet());
+/* app.use(helmet()); */
 app.use(expressSanitizer());
-app.use(cors());
 
 app.use('/api', Router);
 
