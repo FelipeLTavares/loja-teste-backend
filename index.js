e56fedf69b3cd8446eb34a92ed336f5c14f28db8
Felipe L Tavares <felipelimatavares96@gmail.com>
2022-04-22 22:58:52 -0300

    8

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import expressSanitizer from 'express-sanitizer';
-import rateLimit from 'express-rate-limit';
 import 'dotenv/config';
 
 import Router from './router.js';
-
-//Limiter
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
 
 //Server
 const app = express();
@@ -22,8 +13,7 @@ app.use(express.json({limit: '4mb'}));
 app.use(express.urlencoded({extended: true, limit: '4mb'}));
 app.use(helmet());
 app.use(expressSanitizer());
-app.use(limiter);
-app.use(cors({origin: true}));
+app.use(cors());
 
 app.use('/api', Router);
 
