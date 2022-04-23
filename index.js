ac35a20e7b5e492b10b63e6106d4dfcadf849e6d
Felipe L Tavares <felipelimatavares96@gmail.com>
2022-04-22 22:48:11 -0300

    6

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ app.use(helmet());
 app.use(expressSanitizer());
 app.use(limiter);
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', /* 'http://localhost:3000' */ 'https://loja-teste-frontend.vercel.app/' );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-  return next();
-});
 
 app.use('/api', Router);
 
